96882a9fae0ffc852d9939b2cfa6350bc0ff03e5
MagickWorX <kouichi.abe.wall@magickworx.com>
2016-01-28 23:19:55 +0900

    Initial Commit

---
+++ CalendarKit/CalendarKit.h
@@ -0,0 +1,19 @@
+//
+//  CalendarKit.h
+//  CalendarKit
+//
+//  Created by Kouichi ABE (WALL) on 2016/01/28.
+//  Copyright © 2016年 Kouichi ABE (WALL). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CalendarKit.
+FOUNDATION_EXPORT double CalendarKitVersionNumber;
+
+//! Project version string for CalendarKit.
+FOUNDATION_EXPORT const unsigned char CalendarKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CalendarKit/PublicHeader.h>
+
+
